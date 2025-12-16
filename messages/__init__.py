98271e4fdd51fae62e2970564faad74a82e35d40
Odilbek <=>
2025-12-16 10:49:38 +0500

    c

--- messages/__init__.py
+++ messages/__init__.py
@@ -1,4 +1,4 @@
-from . import register, check_test, manage_test
+from . import register, check_test, manage_test, manage_db
 from aiogram import Router
 
 router = Router()
@@ -6,3 +6,4 @@ router = Router()
 router.include_router(register.router)
 router.include_router(check_test.router)
 router.include_router(manage_test.router)
+router.include_router(manage_db.router)
