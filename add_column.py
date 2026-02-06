ee22d3c8c3b0a4233d06000458654fafbc24af5f
Odilbek <=>
2026-02-06 21:58:30 +0500

    c

--- add_column.py
+++ add_column.py
@@ -6,8 +6,9 @@ async def init():
         db_url="sqlite:///db.sqlite3",
         modules={"models": ["utils.models"]}
     )
-    cursor = Tortoise.get_connection("default").cursor()
-    await cursor.execute("ALTER TABLE tests ADD COLUMN test_code VARCHAR(20)")
+    conn = Tortoise.get_connection("default")
+    await conn.execute_script("ALTER TABLE tests ADD COLUMN test_code VARCHAR(20)")
+    print("Column 'test_code' added successfully.")
 
 if __name__ == "__main__":
     run_async(init())
